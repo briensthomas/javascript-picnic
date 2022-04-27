93858b95af1b93003fe25d9f848c7710a7a06c4a
Brien Thomas <thomasbrien94@gmail.com>
2022-04-27 11:40:56 -0700

    added button to add/remove items randomly to picnic

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const grapes = document.getElementById('grapes');
 const bread = document.getElementById('bread');
 const watermelon = document.getElementById('watermelon');
 
+const button = document.getElementById('button');
+
 soda.addEventListener('click', () => {
     soda.classList.toggle('picked');
 });
@@ -22,4 +24,19 @@ bread.addEventListener('click', () => {
 
 watermelon.addEventListener('click', () => {
     watermelon.classList.toggle('picked');
+});
+
+button.addEventListener('click', () => {
+    const randomNumber = Math.ceil(Math.random() * 5);
+    if (randomNumber === 1) {
+        soda.classList.toggle('picked');
+    } else if (randomNumber === 2) {
+        cheese.classList.toggle('picked');
+    } else if (randomNumber === 3) {
+        grapes.classList.toggle('picked');
+    } else if (randomNumber === 4) {
+        bread.classList.toggle('picked');
+    } else {
+        watermelon.classList.toggle('picked');
+    }
 });
